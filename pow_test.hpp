44c10eee3fd8c792a0e0770ce3c90103a1c5eec3
dlam022 <dericklam2@gmail.com>
2021-11-05 01:31:29 -0700

    updated

--- pow_test.hpp
+++ pow_test.hpp
@@ -5,6 +5,7 @@
 
 #include "op.hpp"
 #include "pow.hpp"
+#include "add.hpp"
 
 
 TEST(powTest ,simplePow) {
@@ -16,6 +17,50 @@ TEST(powTest ,simplePow) {
 	EXPECT_EQ(test->evaluate(), 9);
 }
 
+TEST(powTest, bigPow) {
+	Op* a = new Op(15);
+	Op* b = new Op(7);
+	
+	Pow* test = new Pow(a,b);
+	
+	EXPECT_EQ(test->evaluate(), 170859375);
+}
+
+TEST(powTest, zeroPow) {
+	Op* a = new Op(20);
+	Op* b = new Op(0);
+	
+	Pow* test = new Pow(a,b);
+
+	EXPECT_EQ(test->evaluate(), 1);
+}
+
+TEST(powTest, negPow) {
+	Op* a = new Op(2);
+	Op* b = new Op(-2);
+	
+	Pow* test = new Pow(a,b);
+
+	EXPECT_EQ(test->evaluate(), 0.25);
+}
+
+TEST(powTest, negNumPosExp) {
+	Op* a = new Op(-2);
+	Op* b = new Op(2);
+
+	Pow* test = new Pow(a,b);
+
+	EXPECT_EQ(test->evaluate(), 4);
+}
+
+TEST(powTest, strPow) {
+	Op* a = new Op(2);
+	Op* b = new Op(4);
+
+	Pow* test = new Pow(a,b);
+	
+	EXPECT_EQ(test->stringify(), "(2.000000 ** 4.000000)");
+}
 
 
 #endif
