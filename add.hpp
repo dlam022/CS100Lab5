2a2e47102615300f97f11cf02dcb1aa9f4e52034
dlam022 <dericklam2@gmail.com>
2021-11-05 00:38:53 -0700

    Updated

--- add.hpp
+++ add.hpp
@@ -1,28 +1,42 @@
 #ifndef __ADD_HPP_
 #define __ADD_HPP_
 
-#include "base.hpp"
 #include "op.hpp"
-#include <string>
-
 
 using namespace std;
 
 class Add : public Base {
-	private:
-		Op* l;
-		Op* r;
+//	private:
+	//	double leftVal;
+	//	double rightVal;
+	//	string leftStr;
+	//	string rightStr;
+	//	Op* l;
+	//	Op* r;
 	public:
-		Add(Op* left, Op* right) {
+		Add(Op* left, Op* right){
 			this-> l = left;
 			this-> r = right;
+		//	leftVal = left->evaluate();
+		//	rightVal = right->evaluate();
+		//	leftStr = left->stringify();
+		//	rightStr = right->stringify();
 		}
 		virtual double evaluate() {
 			return l->evaluate() + r->evaluate();
+		//	return (leftVal + rightVal);
 		}
 		virtual string stringify() {
-			return "(" + l->stringify() + "+" + r->stringify() + ")";
-
+			return "(" + l->stringify() + " + " + r->stringify() + ")";
+		//	return ("(" + leftStr + " + " + rightStr + ")" );
+		}
+	private:
+	//	double leftVal;
+	//	double rightVal;
+	//	string leftStr;
+	//	string rightStr;
+		Op* l;
+		Op* r;
 };
 
 #endif
